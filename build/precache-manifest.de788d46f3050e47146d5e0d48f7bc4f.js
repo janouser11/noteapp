5a291b9690358d1579492145fc9ebf19d7042d1a
janouser11 <janouser11@gmail.com>
2019-04-04 19:51:33 -0400

    Removed build from .gitignore to work with heroku

---
+++ build/precache-manifest.de788d46f3050e47146d5e0d48f7bc4f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "f587534ef869e13572a3",
+    "url": "/static/js/main.ba734be7.chunk.js"
+  },
+  {
+    "revision": "ef3ca680b8b68a82a1f0",
+    "url": "/static/js/2.56207ed4.chunk.js"
+  },
+  {
+    "revision": "f587534ef869e13572a3",
+    "url": "/static/css/main.5291f791.chunk.css"
+  },
+  {
+    "revision": "47b5a8a7d376c81406bc7634f1303c75",
+    "url": "/index.html"
+  }
+];
